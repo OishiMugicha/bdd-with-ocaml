73300f2148fcafd17bc6b6faf56ec70cf91597bf
OishiMugicha <yutaro.st1192@gmail.com>
2025-11-30 22:06:35 +0900

    二項演算の適用applyのナイーブな実装

--- bdd.ml
+++ bdd.ml
@@ -44,6 +44,26 @@ module BDD = struct
   
   (** Algebraic operations **)
 
+  (* 二項演算の適用（ナイーブな実装） *)
+  let rec apply : (bool -> bool -> bool) -> t -> t -> t = fun op f g ->
+    let new_vertex : int -> t -> t -> t = fun k w0 w1 ->
+      if w0 = w1 then w0 else V(k, w0, w1)
+    in
+    match (f, g) with
+      | (Zero, Zero) -> const (op false false)
+      | (Zero, One ) -> const (op false true )
+      | (One , Zero) -> const (op true  false)
+      | (One , One ) -> const (op true  true )
+      | (Zero, V(j, g0, g1)) | (One, V(j, g0, g1)) -> new_vertex j (apply op f g0) (apply op f g1)
+      | (V(i, f0, f1), Zero) | (V(i, f0, f1), One) -> new_vertex i (apply op f0 g) (apply op f1 g)
+      | (V(i, f0, f1), V(j, g0, g1)) ->
+            if i < j then new_vertex i (apply op f0 g) (apply op f1 g)
+            else if i > j then new_vertex j (apply op f g0) (apply op f g1)
+            else new_vertex i (apply op f0 g0) (apply op f1 g1)
 
+  (* let not : t -> t
+  let and_ : t -> t -> t
+  let or_ : t -> t -> t
+  let xor_ : t -> t -> t *)
 
 end
