9c9be343225aa92b15dc0f78745102c34b094669
OishiMugicha <yutaro.st1192@gmail.com>
2025-11-30 23:12:56 +0900

    testを実装

--- bdd.ml
+++ bdd.ml
@@ -1,48 +1,56 @@
-(* BDD module の signature *)
+(* BDD の signature *)
 module type BDDType =
 sig
-  type t
+  (* typedef *)
+  type t =
+    | Zero
+    | One
+    | V of int * t * t (* (index, lo, hi) *)
+
   (* Base functions *)
   val const : bool -> t
   val var : int -> t
+
   (* Algebraic operations *)
   val apply : (bool -> bool -> bool) -> t -> t -> t
   val not_ : t -> t
   val and_ : t -> t -> t
   val or_ : t -> t -> t
   val xor_ : t -> t -> t
+
   (* Nonalgebraic operations *)
   val restrict : t -> int -> bool -> t
   val compose : t -> int -> t -> t
   val exists : t -> int list -> t
   val forall : t -> int list -> t
   val relprod : t -> t -> int list -> t
+
   (* Examining functions *)
   val equal : t -> t -> bool
   val eval : t -> int list -> bool
   val satisfy : t -> bool
 end
 
-(* BDD の実装 *)
-module BDD : BDDType = struct
 
+(* BDD の実装 *)
+module BDD : BDDType = 
+struct
   (* BDD の型定義 *)
   type t =
     | Zero
     | One
     | V of int * t * t (* (index, lo, hi) *)
-  
 
   (** Base functions **)
-  
+
   (* 定数 *)
   let const : bool -> t = fun b ->
     if b then One else Zero
-  
+
   (* 変数 *)
   let var : int -> t = fun i ->
     V (i, Zero, One)
-  
+
 
   (** Algebraic operations **)
 
@@ -94,7 +102,7 @@ module BDD : BDDType = struct
   (* composition *)
   let compose : t -> int -> t -> t = fun f i g ->
     or_ (and_ g (restrict f i true)) (and_ (not_ g) (restrict f i false))
-  
+
   (* existential *)
   let rec exists : t -> int list -> t = fun f is ->
     match is with
@@ -111,12 +119,12 @@ module BDD : BDDType = struct
   let relprod : t -> t -> int list -> t = fun f g is ->
     exists (and_ f g) is
 
-  
+
   (** Examining functions **)
-  
+
   (* reduce である場合、equality は単に "=" *)
   let equal : t -> t -> bool = (=)
-  
+
   (* evaluation *)
   let rec eval : t -> int list -> bool = fun f is ->
     match f with
@@ -126,5 +134,78 @@ module BDD : BDDType = struct
 
   (* satisfiability *)
   let satisfy : t -> bool = fun f -> not (equal Zero f)
-
 end
+
+
+(****** テスト ******)
+
+open BDD
+
+let test_const_true () =
+  assert( equal (const true) One )
+
+let test_const_false () =
+  assert( equal (const false) Zero )
+
+let test_var_0 () =
+  assert( equal (var 0) (V (0, Zero, One)) )
+
+let test_and_true_and_true () =
+  assert( equal (and_ (const true) (const true)) (const true) )
+
+let test_and_true_and_false () =
+  assert( equal (and_ (const true) (const false)) (const false) )
+
+let test_or_true_and_false () =
+  assert( equal (or_ (const true) (const false)) (const true) )
+
+let test_or_false_and_false () =
+  assert( equal (or_ (const false) (const false)) (const false) )
+
+let test_not_true () =
+  assert( equal (not_ (const true)) (const false) )
+
+let test_not_false () =
+  assert( equal (not_ (const false)) (const true) )
+
+let test_xor_true_and_false () =
+  assert( equal (xor_ (const true) (const false)) (const true) )
+
+let test_exists_on_single_variable () =
+  let bdd = var 0 in
+  assert( equal (exists bdd [0]) One )
+
+let test_forall_on_single_variable () =
+  let bdd = var 0 in
+  assert( equal (forall bdd [0]) Zero )
+
+let test_restrict_true_on_variable () =
+  let bdd = var 0 in
+  assert( equal (restrict bdd 0 true) One )
+
+let test_restrict_false_on_variable () =
+  let bdd = var 0 in
+  assert( equal (restrict bdd 0 false) Zero )
+
+let test_relprod_of_two_vars () =
+  let bdd1 = var 0 in
+  let bdd2 = var 1 in
+  assert( equal (relprod bdd1 bdd2 [0; 1]) (and_ bdd1 bdd2) )
+
+let test () =
+    test_const_true ();
+    test_const_false ();
+    test_var_0 ();
+    test_and_true_and_true ();
+    test_and_true_and_false ();
+    test_or_true_and_false ();
+    test_or_false_and_false ();
+    test_not_true ();
+    test_not_false ();
+    test_xor_true_and_false ();
+    test_exists_on_single_variable ();
+    test_forall_on_single_variable ();
+    test_restrict_true_on_variable ();
+    test_restrict_false_on_variable ();
+    test_relprod_of_two_vars ();
+    print_endline "All tests passed!"
